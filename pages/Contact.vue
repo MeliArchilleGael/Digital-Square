1e8380f63a35d8073897f658315a57ec2aaf648a
Gael <meliarchille@gmail.com>
2022-07-27 10:20:04 +0100

    update contact

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -17,21 +17,21 @@
               <div class="info-box">
                 <i class="bx bx-map"></i>
                 <h3>Our Address</h3>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <p>Carrefour Emia, Yaoundé-CMR</p>
               </div>
             </div>
             <div class="col-md-6">
               <div class="info-box mt-4">
                 <i class="bx bx-envelope"></i>
                 <h3>Email Us</h3>
-                <p>info@example.com<br>contact@example.com</p>
+                <p>service@bizconsulting-guyane.com</p>
               </div>
             </div>
             <div class="col-md-6">
               <div class="info-box mt-4">
                 <i class="bx bx-phone-call"></i>
                 <h3>Call Us</h3>
-                <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                <p>+594 694 142 053 <br> +237 671 835 516</p>
               </div>
             </div>
           </div>
